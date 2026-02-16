cf707d27d49797ef79afbd671ff02d7fdeab2476
NijasTp <sns11163@gmail.com>
2026-02-16 14:13:19 +0530

    fix(client): axios issue fix

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -57,48 +57,37 @@ api.interceptors.response.use(
       return Promise.reject(error);
     }
 
-    if (
-      status === 401 &&
-      !originalRequest._retry &&
-      originalRequest.url !== "/user/refresh-token"
-    ) {
-      originalRequest._retry = true;
-      try {
-        await api.post("/user/refresh-token", {}, { withCredentials: true });
-        return api(originalRequest);
-      } catch (refreshError) {
-        const state = store.getState();
-        let role: string | null = null;
+    if (status === 401) {
+      const state = store.getState();
+      let role: string | null = null;
 
-        if (state.userAuth.user) role = "user";
-        else if (state.trainerAuth.trainer) role = "trainer";
-        else if (state.gymAuth.gym) role = "gym";
-        else if (state.adminAuth.admin) role = "admin";
+      if (state.userAuth.user) role = "user";
+      else if (state.trainerAuth.trainer) role = "trainer";
+      else if (state.gymAuth.gym) role = "gym";
+      else if (state.adminAuth.admin) role = "admin";
 
-        switch (role) {
-          case "user":
-            store.dispatch(userLogout());
-            window.location.href = "/login";
-            break;
-          case "trainer":
-            store.dispatch(logoutTrainer());
-            window.location.href = "/trainer/login";
-            break;
-          case "gym":
-            store.dispatch(logoutGym());
-            window.location.href = "/gym/login";
-            break;
-          case "admin":
-            store.dispatch(adminLogout());
-            window.location.href = "/admin/login";
-            break;
-          default:
-            window.location.href = "/login";
-            break;
-        }
-
-        return Promise.reject(error);
+      switch (role) {
+        case "user":
+          store.dispatch(userLogout());
+          window.location.href = "/login";
+          break;
+        case "trainer":
+          store.dispatch(logoutTrainer());
+          window.location.href = "/trainer/login";
+          break;
+        case "gym":
+          store.dispatch(logoutGym());
+          window.location.href = "/gym/login";
+          break;
+        case "admin":
+          store.dispatch(adminLogout());
+          window.location.href = "/admin/login";
+          break;
+        default:
+          window.location.href = "/login";
+          break;
       }
+      return Promise.reject(error);
     }
 
     return Promise.reject(error);
